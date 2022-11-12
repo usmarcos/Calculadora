7f18b06ebcbb7df1517552be761b6dc1c239b9d1
Marcos V <marcosvinicius@employer.com.br>
2022-11-11 21:51:51 -0300

    teste

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Calculadora {
+import java.util.Scanner;
+
+public class Calculadora {
+    public Calculadora() {
+    }
+
+    public static void main(String[] args) {
+        double valorUm;
+        double valorDois;
+        double resultado = 0;
+        Scanner scann = new Scanner(System.in);
+
+        System.out.println("Escolha uma das operações");
+        int indexOp;
+        do {
+            System.out.println("Escolha uma das operações");
+            System.out.println("0 - Soma");
+            System.out.println("1 - Subtração");
+            System.out.println("2 - Multiplicação");
+            System.out.println("3 - Divisão");
+            indexOp = scann.nextInt();
+            if (indexOp < 0 || indexOp > 3) {
+                System.out.println("Operação inexistente\n");
+            }
+        } while (indexOp < 0 || indexOp > 3);
+
+        System.out.println("Insira o primeiro número: ");
+        valorUm = scann.nextDouble();
+        System.out.println("Insira o segundo número: ");
+        valorDois = scann.nextDouble();
+        switch (indexOp) {
+            case 0:
+                resultado = valorUm + valorDois;
+                break;
+            case 1:
+                resultado = valorUm - valorDois;
+                break;
+            case 2:
+                resultado = valorUm * valorDois;
+                break;
+            case 3:
+                resultado = valorUm / valorDois;
+        }
+
+        System.out.println("Resultado da operação " + valorUm + " e " + valorDois + " = " + resultado);
+        scann.close();
+    }
 }
+
--- src/CalculadoraArgs.java
+++ src/CalculadoraArgs.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class CalculadoraArgs {
+public class CalculadoraArgs {
+    public static void main(String[] args) {
+        double valor1 = Double.valueOf(args[0]);
+        double valor2 = Double.valueOf(args[1]);
+        double resultado = valor1 + valor2;
+        System.out.println("Resultado da soma de " + valor1 + " e " + valor2 + " = " + resultado);
+    }
 }
